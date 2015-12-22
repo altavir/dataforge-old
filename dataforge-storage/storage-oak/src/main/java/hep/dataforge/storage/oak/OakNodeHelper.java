15962c4b541b30c72fa47e59888d56823197cf94
darksnake <darksnake@LUNA>
2015-12-22 15:29:30 +0300

    [no commit message]

---
+++ dataforge-storage/storage-oak/src/main/java/hep/dataforge/storage/oak/OakNodeHelper.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hep.dataforge.storage.oak;
+
+import org.apache.jackrabbit.oak.api.Tree;
+
+/**
+ *
+ * @author Alexander Nozik <altavir@gmail.com>
+ */
+public class OakNodeHelper {
+    private Tree node;
+    
+}
---
+++ dataforge-storage/storage-oak/src/main/java/hep/dataforge/storage/oak/OakStorage.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hep.dataforge.storage.oak;
+
+import hep.dataforge.exceptions.StorageException;
+import hep.dataforge.meta.Meta;
+import hep.dataforge.storage.api.Loader;
+import hep.dataforge.storage.api.Storage;
+import hep.dataforge.storage.commons.AbstractStorage;
+
+/**
+ *
+ * @author Alexander Nozik <altavir@gmail.com>
+ */
+public class OakStorage extends AbstractStorage {
+
+    public OakStorage(Storage parent, String name, Meta annotation) {
+        super(parent, name, annotation);
+    }
+
+    public OakStorage(String name, Meta annotation) {
+        super(name, annotation);
+    }
+
+    public OakStorage(String name) {
+        super(name);
+    }
+    
+    
+
+    @Override
+    public Loader buildLoader(Meta loaderConfiguration) throws StorageException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Storage buildShelf(String shelfName, Meta shelfConfiguration) throws StorageException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
