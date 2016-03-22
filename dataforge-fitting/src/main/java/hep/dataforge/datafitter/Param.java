080fc31baac20c02e86e494e5041404b291384e0
darksnake <darksnake@LUNA>
2016-03-22 16:57:22 +0300

    [no commit message]

--- dataforge-fitting/src/main/java/hep/dataforge/datafitter/Param.java
+++ dataforge-fitting/src/main/java/hep/dataforge/datafitter/Param.java
@@ -245,6 +245,8 @@ public class Param {
                 digits = digits - (int) (Math.log10(err)) + 3;
             }
 
+            digits = Math.max(digits, 1);
+            
             if (isConstrained()) {
                 return String.format("'%s'\t= %." + digits + "g \u00b1 %.2g\t(%g,%g)", this.name, this.value, this.err, this.lowerBound, this.upperBound);
             } else {
