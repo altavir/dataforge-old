23e03b10c75d91f89eb103316d420a3a368541bc
Alexander Nozik <altavir@gmail.com>
2016-06-23 15:37:41 +0300

    [no commit message]

--- dataforge-core/src/main/java/hep/dataforge/workspace/TaskBuilder.java
+++ dataforge-core/src/main/java/hep/dataforge/workspace/TaskBuilder.java
@@ -6,12 +6,12 @@
 package hep.dataforge.workspace;
 
 import hep.dataforge.actions.Action;
+import hep.dataforge.actions.ActionManager;
 import static hep.dataforge.actions.ActionUtils.ACTION_NODE_KEY;
 import static hep.dataforge.actions.ActionUtils.ACTION_TYPE;
 import static hep.dataforge.actions.ActionUtils.SEQUENCE_ACTION_TYPE;
 import static hep.dataforge.actions.ActionUtils.buildAction;
 
-import hep.dataforge.actions.GenericAction;
 import hep.dataforge.context.Context;
 import hep.dataforge.context.ProcessManager;
 import hep.dataforge.data.Data;
@@ -22,7 +22,7 @@ import hep.dataforge.utils.GenericBuilder;
 import hep.dataforge.utils.MetaFactory;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 import javafx.util.Pair;
@@ -38,11 +38,41 @@ public class TaskBuilder implements GenericBuilder<Task, TaskBuilder> {
     /**
      * A list of actions inside task
      */
-    private final List<Pair<Action, MetaFactory<Meta>>> actions = new ArrayList<>();
+    private final List<Pair<Function<Context, Action>, MetaFactory<Meta>>> actions = new ArrayList<>();
     /**
      * Transformations of TaskModel
      */
     private final List<ModelTransformation> modelTransformations = new ArrayList<>();
+
+    /**
+     * Lazy action initialization from context ActionManager
+     *
+     * @param action
+     * @param metaBuilder
+     * @return
+     */
+    public TaskBuilder doLast(String actionName, MetaFactory<Meta> metaBuilder) {
+        actions.add(new Pair<>(ctx -> ActionManager.buildFrom(ctx).getAction(actionName), metaBuilder));
+        return self();
+    }
+
+    /**
+     * Lazy action initialization from Class default constructor
+     *
+     * @param action
+     * @param metaBuilder
+     * @return
+     */
+    public TaskBuilder doLast(Class<? extends Action> actionClass, MetaFactory<Meta> metaBuilder) {
+        actions.add(new Pair<>(ctx -> {
+            try {
+                return actionClass.newInstance();
+            } catch (InstantiationException | IllegalAccessException ex) {
+                throw new RuntimeException("Can't initialize action", ex);
+            }
+        }, metaBuilder));
+        return self();
+    }
 
     /**
      * Add last action with given transformation from task meta to specific
@@ -53,12 +83,14 @@ public class TaskBuilder implements GenericBuilder<Task, TaskBuilder> {
      * @return
      */
     public TaskBuilder doLast(Action action, MetaFactory<Meta> metaBuilder) {
-        actions.add(new Pair<>(action, metaBuilder));
+        actions.add(new Pair<>(ctx -> action, metaBuilder));
         return self();
     }
 
     /**
-     * Perform an action using given meta as a template and actual task meta as template data
+     * Perform an action using given meta as a template and actual task meta as
+     * template data
+     *
      * @param action
      * @param actionMeta
      * @return
@@ -69,10 +101,11 @@ public class TaskBuilder implements GenericBuilder<Task, TaskBuilder> {
 
     /**
      * Perform action without meta
+     *
      * @param action
      * @return
      */
-    public TaskBuilder doLast(Action action){
+    public TaskBuilder doLast(Action action) {
         return doLast(action, (ctx, meta) -> Meta.empty());
     }
 
@@ -84,7 +117,7 @@ public class TaskBuilder implements GenericBuilder<Task, TaskBuilder> {
      * @return
      */
     public TaskBuilder doFirst(Action action, MetaFactory<Meta> metaBuilder) {
-        actions.add(0, new Pair<>(action, metaBuilder));
+        actions.add(0, new Pair<>(ctx -> action, metaBuilder));
         return self();
     }
 
@@ -94,7 +127,8 @@ public class TaskBuilder implements GenericBuilder<Task, TaskBuilder> {
 
     /**
      * Apply model transformation during model preparation phase.
-     * <strong> WARNING: The order of transformation could affect the result. </strong>
+     * <strong> WARNING: The order of transformation could affect the result.
+     * </strong>
      *
      * @param transformation
      * @return
@@ -103,47 +137,62 @@ public class TaskBuilder implements GenericBuilder<Task, TaskBuilder> {
         this.modelTransformations.add(transformation);
         return self();
     }
-    
+
     /**
      * Add default dependency of task being built on given Task.
+     *
      * @param taskName
      * @param as
-     * @param metaTransformation a transformation to extract dependency meta 
-     * @return 
+     * @param metaTransformation a transformation to extract dependency meta
+     * @return
      */
-    public TaskBuilder dependsOnTask(String taskName, String as, UnaryOperator<Meta> metaTransformation) {
+    public TaskBuilder dependsOn(String taskName, String as, UnaryOperator<Meta> metaTransformation) {
         return dependencyRule((Workspace workspace, Meta taskMeta, TaskModel model) -> {
             Meta depMeta = metaTransformation.apply(taskMeta);
             workspace.getTask(taskName).model(workspace, depMeta);
             model.dependsOn(workspace.getTask(taskName).model(workspace, depMeta), as);
         });
     }
-    
+
     /**
-     * Add dependency on specific data. Name patterns are not allowed
+     * Add dependency on specific data. Name patterns are not allowed.
+     *
      * @param dataName
      * @param as
-     * @return 
+     * @return
      */
-    public TaskBuilder dependsOnData(String dataName, String as){
+    public TaskBuilder data(String dataName, String as) {
         return dependencyRule((Workspace workspace, Meta taskMeta, TaskModel model) -> {
             model.dependsOnData(dataName, as);
         });
     }
-    
+
     /**
-     * Add dependency on data group using given pattern
-     * @param pattern
-     * @return 
+     * Add dependency on data group using given pattern.
+     *
+     * @param predicate
+     * @return
      */
-    public TaskBuilder dependsOnData(Predicate<Pair<String,Data<?>>> pattern){
+    public TaskBuilder data(Predicate<Pair<String, Data<?>>> predicate) {
         return dependencyRule((Workspace workspace, Meta taskMeta, TaskModel model) -> {
             workspace.getDataStage().dataStream()
-                    .filter(pattern)
+                    .filter(predicate)
                     .forEach(pair -> model.dependsOnData(pair.getKey()));
         });
     }
-    
+
+    /**
+     * Add all data with name matching any of presented patterns. Patterns could contain ? and * wildcards
+     * @param namePattern
+     * @return 
+     */
+    public TaskBuilder data(String... namePattern) {
+        Predicate<Pair<String, Data<?>>> pattern = pair -> false;
+        for (String n : namePattern) {
+            pattern = pattern.or(pair -> pair.getKey().matches(n.replace("?", ".?").replace("*", ".*?")));
+        }
+        return data(pattern);
+    }
 
     /**
      * apply action configuration from meta node
@@ -171,6 +220,7 @@ public class TaskBuilder implements GenericBuilder<Task, TaskBuilder> {
     }
 
     public interface ModelTransformation {
+
         void apply(Workspace workspace, Meta taskMeta, TaskModel model);
     }
 
@@ -179,8 +229,8 @@ public class TaskBuilder implements GenericBuilder<Task, TaskBuilder> {
         @Override
         protected TaskState transform(ProcessManager.Callback callback, Context context, TaskState state, Meta config) {
             DataNode res = state.getData();
-            for (Pair<Action, MetaFactory<Meta>> pair : actions) {
-                Action action = pair.getKey();
+            for (Pair<Function<Context, Action>, MetaFactory<Meta>> pair : actions) {
+                Action action = pair.getKey().apply(context);
                 Meta actionMeta = pair.getValue().build(context, config);
                 res = action.withParentProcess(callback.processName()).run(context, res, actionMeta);
                 if (actionMeta.hasValue("stageName")) {
