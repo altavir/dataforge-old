3c3e128af7785168ec70b22eb84abfe65ba5978f
Alexander Nozik <altavir@gmail.com>
2017-08-27 22:07:33 +0300

    Table GUI in progress

---
+++ kodex/kodex-fx/src/main/kotlin/hep/dataforge/kodex/fx/KodexFX.kt
@@ -0,0 +1,10 @@
+package hep.dataforge.kodex.fx
+
+import hep.dataforge.tables.Table
+import hep.dataforge.values.Values
+import javafx.scene.control.TableView
+
+
+fun TableView<Values>.table(table: Table){
+
+}
